a05277505c4790ca8da15db03e6b81a7b51f118a
katarushanmukhassai <katarushanmukhasai@gmail.com>
2025-09-08 12:41:44 +0530

    intros

--- intro.java
+++ intro.java
@@ -1,3 +1,4 @@
 // today we installed gitbash, javaidk 21 and vs code
 //create a repository and open vs code and clone the repository
-//come to source control and do stage changes, click on "+" and  do commit
+//come to source control and do stage changes, click on "+" and u will find repositories click it add name in changes and do commit
+// and add some name and commit and now sync chnages
