51607e5cf496573a6c792901e6cc580e4ef16285
katarushanmukhassai <katarushanmukhasai@gmail.com>
2025-09-08 12:38:54 +0530

    1st intro

---
+++ intro.java
@@ -0,0 +1,3 @@
+// today we installed gitbash, javaidk 21 and vs code
+//create a repository and open vs code and clone the repository
+//come to source control and do stage changes, click on "+" and  do commit
