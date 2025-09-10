e40287df5f2935dcb07b9df5bdbae0529ecf1fd4
katarushanmukhassai <katarushanmukhasai@gmail.com>
2025-09-10 11:56:35 +0530

    variabledemo

---
+++ variableDemo.java
@@ -0,0 +1,13 @@
+public class variableDemo{
+    public static void main(String[] args){
+        System.out.println("Welcome to java");
+        int price = 74000;
+        double rating = 4.5;
+        String brand = "Samsung";
+        System.out.println(price);
+        System.out.println(rating);
+        System.out.println(brand);
+    }
+}
+// to check whether the code is working or not use " javac variableDemo.java ""-> javac filename
+//to run the code for output "java variableDemo" -> java classnameS
